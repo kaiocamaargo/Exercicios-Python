1e0ea3337c956a669ef0a5f5361b1982648916f2
Kaio Camargo <kaiocamargojr@gmail.com>
2025-09-24 17:11:30 -0300

    Exercicio finalizado

---
+++ Exercícios/MediaAtitmetica.py
@@ -0,0 +1,5 @@
+#Desenvolva um programa que leia duas notas de um aluno, calcule e mostre a sua média.
+nota1 = float(input('Digite a primeira nota'))
+nota2 = float(input('Digite a segunda nota'))
+media  = (nota1 + nota2) / 2
+print('A média entre a nota {} e da nota {} é {}'.format (nota1, nota2, media))
